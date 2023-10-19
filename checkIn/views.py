201e7eac8c2842767634f63fda40d62c8d4ab74d
Kemosame <matiabeya2@gmail.com>
2023-10-19 11:53:49 -0500

    all files updated

---
+++ checkIn/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render,get_object_or_404
+from .models import Client, Employee, Note
+
+# Create your views here.
+def add_note(request, client_id):
+    client = get_object_or_404(Client, pk=client_id)
+    if request.method == 'POST':
+        text = request.POST.get('text')
+        employee = request.user
+        clock_in = request.POST.get('clock_in')
+        clock_out = request.POST.get('clock_out')
+        Note.objects.create(client = client, text = text, employee = employee, clock_in = clock_in, clock_out = clock_out)
+        #handle form submission and create and new note
+    return render(request, 'add_note.html',{'client':client})
+
+def client_notes(request, client_id):
+    client = get_object_or_404(Client, pk = client_id)
+    notes = Note.objects.filter(client=client).order_by('-timestamp')
+    context = {'notes': notes}
+    return render(request,'client_notes.html',context)
