724fc223de4f8c482284763f54d08b18a2446c7b
Kemosame <matiabeya2@gmail.com>
2023-10-28 17:57:08 -0500

    changes to models and views.

--- checkIn/models.py
+++ checkIn/models.py
@@ -2,23 +2,32 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+
+
 class Client(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=20, blank=True)
     address = models.TextField()
 
+
 class Employee(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=20, blank=True)
     address = models.TextField()
-    
+
 
 class Note(models.Model):
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
     text = models.TextField()
     employee = models.ForeignKey(User, on_delete=models.CASCADE)
-    timestamp = models.DateTimeField(auto_now_add = True)
+    timestamp = models.DateTimeField(auto_now_add=True)
     clock_in = models.TimeField()
     click_out = models.TimeField()
+
+
+class Login(models.Model):
+    username = models.CharField(max_length=100, unique=True)
+    password = models.CharField(max_length=255)
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
--- checkIn/views.py
+++ checkIn/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render,get_object_or_404
-from .models import Client, Employee, Note
+from django.shortcuts import render, get_object_or_404
+from .models import Client, Employee, Note, Login
 
 # Create your views here.
+
+
+class CustomLoginView(Login):
+    template_name = 'account/login.html'
+
+
 def add_note(request, client_id):
     client = get_object_or_404(Client, pk=client_id)
     if request.method == 'POST':
@@ -9,12 +15,14 @@ def add_note(request, client_id):
         employee = request.user
         clock_in = request.POST.get('clock_in')
         clock_out = request.POST.get('clock_out')
-        Note.objects.create(client = client, text = text, employee = employee, clock_in = clock_in, clock_out = clock_out)
-        #handle form submission and create and new note
-    return render(request, 'add_note.html',{'client':client})
+        Note.objects.create(client=client, text=text, employee=employee,
+                            clock_in=clock_in, clock_out=clock_out)
+        # handle form submission and create and new note
+    return render(request, 'add_note.html', {'client': client})
+
 
 def client_notes(request, client_id):
-    client = get_object_or_404(Client, pk = client_id)
+    client = get_object_or_404(Client, pk=client_id)
     notes = Note.objects.filter(client=client).order_by('-timestamp')
     context = {'notes': notes}
-    return render(request,'client_notes.html',context)
+    return render(request, 'client_notes.html', context)
